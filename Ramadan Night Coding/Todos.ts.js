76465c264ed0fe4e700e34498cfa6b353f081c11
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-04-17 14:04:57 -0700

    Add files via upload

---
+++ Ramadan Night Coding/Todos.ts.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let todos = []; // Shooper []
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'firstQuestion',
+            message: 'what you want to add in your todos',
+            type: 'input',
+        },
+        {
+            name: 'SecondQuestion',
+            type: 'confirm', // when type is confirm answer is yes or no
+            message: 'would you like to add more in your todos ?',
+            default: 'false'
+        }
+    ]);
+    todos.push(addTask.firstQuestion);
+    // the loop ll be run on base of variables
+    condition = addTask.addMore;
+    console.log(todos);
+}
